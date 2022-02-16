905c50c677a5f59b5f2fa2b15af7fef8a0d0f821
yagnesh77 <nayiyagnesh7@gmail.com>
2022-02-16 12:50:43 +0530

    crispyfromwithclass

--- work2/polls2/forms.py
+++ work2/polls2/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 class ContactForm(forms.Form):
-  name =forms.CharField(max_length=77)
+  name =forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'name'}))
   email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
-  password = forms.CharField(widget=forms.PasswordInput())
+  password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'password'}))
   address_1 = forms.CharField(
         label='Address',
-        widget=forms.TextInput(attrs={'placeholder': '1234 Main St'})
+        widget=forms.TextInput(attrs={'placeholder': 'Address'})
     )
   address_2 = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Apartment, studio, or floor'})
+        widget=forms.TextInput(attrs={'placeholder': 'Apartment, or floor'})
     )
   city = forms.CharField()
   zip_code = forms.CharField(label='Zip')
